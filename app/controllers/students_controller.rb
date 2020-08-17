231dd31a35d4886af82371b0213487c2fb1f2a8a
Abel Kalum <chumbabel@gmail.com>
2020-08-17 20:06:02 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,6 +16,4 @@ class StudentsController < ApplicationController
     @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
     redirect_to student_path(@student)
   end
-  
-
 end
